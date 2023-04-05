5befbb97f516a3a81b6e2ae0bca59fb6d09b2857
Mikpon1 <127319253+Mikpon1@users.noreply.github.com>
2023-04-05 12:17:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int wysokosc = 4; //wysokosc prostokąta
+    int dlugosc = 6; //dlugosc prostokąta
+
+    for (int i = 0; i < wysokosc; i++){
+      for (int j=0; j < dlugosc; j++){
+        System.out.print("*");
+      }
+    
+    System.out.println();
+    }
+    
+    
+  }
+}
